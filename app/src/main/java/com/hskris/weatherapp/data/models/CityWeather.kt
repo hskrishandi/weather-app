2686024d55b329ecfb87e1dcc88d6e2e1fca6f83
Hans <pt.gojekindonesia@PTs-MacBook-Pro-4.local>
2019-07-04 15:58:50 +0700

    Add getID in CityWeather

--- app/src/main/java/com/hskris/weatherapp/data/models/CityWeather.kt
+++ app/src/main/java/com/hskris/weatherapp/data/models/CityWeather.kt
@@ -4,6 +4,10 @@ class CityWeather(private val id: Int, val name: String, val country: String) {
 
     private val forecasts: MutableList<Forecast> = mutableListOf()
 
+    fun getId(): Int {
+        return id
+    }
+
     fun addForecasts(forecast: Forecast){
         forecasts.add(forecast)
     }
