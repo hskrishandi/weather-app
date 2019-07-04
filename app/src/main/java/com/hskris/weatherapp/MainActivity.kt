8ad330c0732837e6fc892e54ded599b64246ce86
Hans <pt.gojekindonesia@PTs-MacBook-Pro-4.local>
2019-07-04 23:25:10 +0700

    Make recycler view adapter for forecast

--- app/src/main/java/com/hskris/weatherapp/MainActivity.kt
+++ app/src/main/java/com/hskris/weatherapp/MainActivity.kt
@@ -2,11 +2,66 @@ package com.hskris.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.hskris.weatherapp.data.models.Forecast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val weathers = listOf(
+            Forecast(Date(1562263200000), 29.6, 75, "Rain", "Light rain"),
+            Forecast(Date(1562349600000), 30.2, 75, "Rain", "Clear"),
+            Forecast(Date(1562436000000), 22.5, 75, "Rain", "Overcast")
+        )
+
+        recyclerViewForecast.layoutManager = LinearLayoutManager(this)
+        recyclerViewForecast.adapter = ForecastAdapter(weathers)
+    }
+}
+
+class ForecastAdapter(val items: List<Forecast>) : RecyclerView.Adapter<ForecastAdapter.ForecastItem>(){
+
+    private val calendar = Calendar.getInstance()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ForecastItem {
+        return ForecastItem(
+            LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.forecast_row, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: ForecastItem, position: Int) {
+        val item = items[position]
+
+        val date = item.date
+        calendar.setTime(date)
+        holder.day.text = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.US)?.toString()
+
+        val temp = item.temp.toInt().toString() + " C"
+        holder.temp.text = temp
+
+        holder.desc.text = item.description
+    }
+
+
+    class ForecastItem(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val day: TextView = itemView.findViewById(R.id.textViewDay)
+        val temp: TextView = itemView.findViewById(R.id.textViewTemp)
+        val desc: TextView = itemView.findViewById(R.id.textViewDescription)
     }
 }
