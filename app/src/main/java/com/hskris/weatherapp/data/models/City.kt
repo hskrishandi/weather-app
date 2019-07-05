193441f40ed2732d8763f124a2340b679a4be79f
Hans <pt.gojekindonesia@PTs-MacBook-Pro-4.local>
2019-07-05 13:44:14 +0700

    Make City model into Parcelable object

--- app/src/main/java/com/hskris/weatherapp/data/models/City.kt
+++ app/src/main/java/com/hskris/weatherapp/data/models/City.kt
@@ -1,3 +1,9 @@
 package com.hskris.weatherapp.data.models
 
-data class City(val id: Int, val name: String)
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class City(val id: Int, val name: String) : Parcelable {
+    constructor() : this(-1, "")
+}
