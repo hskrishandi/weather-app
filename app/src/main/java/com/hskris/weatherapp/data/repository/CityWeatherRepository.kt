70d030ddbcabd1d5e74658875cc6a08b397bd9e4
Hans <pt.gojekindonesia@PTs-MacBook-Pro-4.local>
2019-07-08 15:23:37 +0700

    Modify CityWeatherRepository to focus on fetching from API

--- app/src/main/java/com/hskris/weatherapp/data/repository/CityWeatherRepository.kt
+++ app/src/main/java/com/hskris/weatherapp/data/repository/CityWeatherRepository.kt
@@ -3,14 +3,12 @@ package com.hskris.weatherapp.data.repository
 import android.util.Log
 import com.hskris.weatherapp.data.api.Api
 import com.hskris.weatherapp.data.models.CityWeather
-import com.hskris.weatherapp.data.models.Forecast
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class CityWeatherRepository {
 
-    private val cityId = listOf(1642911, 1277333)
     private val api = Api.getInstance()
 
     fun fetchCityWeathers(id: Int, callback: ForecastCallback){
@@ -21,28 +19,13 @@ class CityWeatherRepository {
             }
 
             override fun onResponse(call: Call<CityWeather>, response: Response<CityWeather>) {
-                val result = getFiveDaysForecast(response.body()!!)
-                callback.onGetForecast(result)
+                callback.onGetForecast(response.body()!!)
             }
 
         })
     }
-
-    fun getFiveDaysForecast(cityWeather: CityWeather): List<Forecast>{
-
-        val forecasts = cityWeather.getForecasts()
-
-        val selectedForecast = mutableListOf<Forecast>()
-        for (i in 0..4) {
-            val index = i * 8
-            if (index < forecasts.size)
-                selectedForecast.add(forecasts[index])
-        }
-
-        return selectedForecast
-    }
 }
 
 interface ForecastCallback {
-    fun onGetForecast(forecasts: List<Forecast>)
+    fun onGetForecast(cityWeather: CityWeather)
 }
